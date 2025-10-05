c918ba6e1db1504253c9fe55fa60244d44797358
byronmckitrick16 <mck24036@byui.edu>
2025-10-05 16:05:05 -0600

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -1,28 +1,53 @@
 function createGrid() {
     // when working on the other difficulties just make rows and cols = a parameter sent in by the addeventlistener to use this function each time no matter what the difficulty is
     let rows = 9;
-    let cols = 7;
+    let colsEven = 7;
+    let colsOdd = 8;
     let grid = [];
 
     for (let r = 0; r < rows; r++) {
         grid[r] = [];
-        for (let c = 0; c < cols; c++) {
-            grid[r][c] = {
-                mine: false,
-                revealed: false,
-                flagged: false,
-                neighbors: 0
-            };
+        if (r % 2 === 0) {
+            for (let c = 0; c < colsEven; c++) {
+                grid[r][c] = {
+                    mine: false,
+                    revealed: false,
+                    flagged: false,
+                    neighbors: 0
+                };
+            }
+
+        }else if (r % 2 === 1) {
+            for (let c = 0; c < colsOdd; c++) {
+                grid[r][c] = {
+                    mine: false,
+                    revealed: false,
+                    flagged: false,
+                    neighbors: 0
+                };
+            }
         }
     }
-    placeMines(grid)
+    placeMines(grid, 30)
 }
 createGrid()
-function placeMines(grid) {
-    
+function placeMines(grid, mineCount) {
+    let placed = 0;
+
+    while (placed < mineCount) {
+        const row = Math.floor(Math.random() * grid.length);
+        const col = Math.floor(Math.random() * grid[row].length);
+        const cell = grid[row][col];
+
+        if (!cell.mine) {
+            cell.mine = true;
+            placed++;
+        }
+    }
+    countNeighbors(grid)
 }
 
-function countNeighbors() {
+function countNeighbors(grid) {
 
 }
 
