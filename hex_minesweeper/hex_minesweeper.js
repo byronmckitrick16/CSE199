348b93ba5cc97a44317d08ffdd219d4f7f865841
byronmckitrick16 <mck24036@byui.edu>
2025-10-28 12:25:23 -0600

    more

--- hex_minesweeper/hex_minesweeper.js
+++ hex_minesweeper/hex_minesweeper.js
@@ -256,7 +256,7 @@ function checkWin() {
 function gameWon() {
     // display a screen telling them they won
     const board = document.querySelector(".board")
-    const winConfetti = '<img class="gameWinConfetti animation" src="confetti.gif" alt="animation for when you win"></img>'
+    const winConfetti = '<img class="gameWinConfetti animation" src="/confetti.gif" alt="animation for when you win"></img>'
     board.insertAdjacentHTML("beforeend", winConfetti)
     revealBoard();
     stopTimer();
@@ -266,7 +266,7 @@ function gameWon() {
 function gameOver() {
     // display a screen telling them they lost and letting them see where all the mines were
     const board = document.querySelector(".board")
-    const loseExplostion = '<img class="loseExplostion animation" src="explosion.gif" alt="animation for when you lose">'
+    const loseExplostion = '<img class="loseExplostion animation" src="/explosion.gif" alt="animation for when you lose">'
     board.insertAdjacentHTML("beforeend", loseExplostion)
     revealBoard();
     stopTimer();
